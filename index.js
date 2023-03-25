bea138ac4e877d21494dd3c55b2953a67f0a24e6
YogeshSahu0 <yogesh.sahu@opensenselabs.com>
2023-03-25 15:30:01 +0530

    adding dynamic port

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ import express from "express"
 
 const app = express();
 
+const port = process.env.PORT || 8000;
+
 app.use("/",(req,res)=>{
     res.send("<h1>man gaya</h1>")
 })
 
-app.listen(5500,()=>{
+app.listen(port,()=>{
     console.log("server is listening at 5500");
 })
