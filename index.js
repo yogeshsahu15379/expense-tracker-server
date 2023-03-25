958d289d14654c6c0a5bd5ad8581aa650facca45
YogeshSahu0 <yogesh.sahu@opensenselabs.com>
2023-03-25 18:39:40 +0530

    minor change

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const port = process.env.PORT || 8000;
 
 app.use("/",(req,res)=>{
-    res.send("<h1>kyu nhi mann rha man ja na bubu</h1>")
+    res.send("<h1>Expense tracker APIs</h1>")
 })
 
 app.listen(port,()=>{
